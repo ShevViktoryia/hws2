59b9a66f9059c007b1a8756b9adf60a5301c1daf
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-03-19 13:26:33 +0300

    feat: add hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,33 @@
 const initState = {
-    themeId: 1,
+  themeId: 1,
+};
+
+const SET_THEME_ID = "SET_THEME_ID";
+
+interface SetThemeIdAction {
+  type: typeof SET_THEME_ID;
+  payload: number;
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
-    }
+interface ThemeState {
+  themeId: number;
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const themeReducer = (
+  state = initState,
+  action: SetThemeIdAction
+): ThemeState => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case SET_THEME_ID:
+      return { ...state, themeId: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const changeThemeId = (id: number): SetThemeIdAction => ({
+  type: "SET_THEME_ID",
+  payload: id,
+}); // fix any
